ab9781842ff829306843055ee7718b3e93ada1b8
omar2205 <omar2205a@yahoo.com>
2023-07-24 14:23:11 +0300

    add cors, add FRONTEND_URL env var

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors((req, cb) => {
+    cb(null, {
+      origin: ['http://localhost:3000', process.env.FRONTEND_URL].includes(
+        req.header('Origin'),
+      ),
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    });
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
